363b838d4cb7eac6712d62c24febec36b840544f
Mahmoud A.Shawqi <117821096+mhmod01110@users.noreply.github.com>
2025-01-25 00:06:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ mongoose.connect(MONGODB_URI, {
 })
     .then((result) => {
         app.listen(PORT, "0.0.0.0", () => {
-            console.log("Server is running on port ${PORT}");
+            console.log("Server is running on port: ", PORT);
         });
     })
     .catch((err) => {
